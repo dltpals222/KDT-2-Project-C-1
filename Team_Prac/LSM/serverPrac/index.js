b98aece720edd70aa4e0fc801ee30d78c9a9228a
leesemin <leesemin222@gmail.com>
2023-04-03 18:51:36 +0900

    아직 뭘 만들지 미지수

---
+++ Team_Prac/LSM/serverPrac/index.js
@@ -0,0 +1,8 @@
+import tagMakeCall from './modules/tagMakeCall.js'
+import classPost from './classPost.js'
+
+const root = document.getElementById('root')
+const name = document.getElementById('ip-name')
+const phone = document.getElementById('ip-phone')
+const email = document.getElementById('ip-email')
+
