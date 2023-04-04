45b2e969f22dc2cac3c47499f3d29f9a7a726ae3
leesemin <leesemin222@gmail.com>
2023-04-04 18:48:25 +0900

    쿼리스트링 안돼서 만든건데 이제 돼서 안쓰게됨 ㅅㅍ

---
+++ Team_Prac/LSM/serverPrac/mysql/makeQuerytring.js
@@ -0,0 +1,41 @@
+// module.exports = {
+//   userDataObject : function(values) {
+//     const firstSplit = values.split('&')
+//     const secondSplit = {};
+//     firstSplit.forEach(element => {
+//       secondSplit.push(element.split('=')[0],element.split('=')[1])
+      
+//     });
+//   }
+// }
+
+
+// function makeQuerystring (values) {
+//   const decoding = decodeURIComponent(values)
+//   const firSpl = decoding.split('&')
+//   const secSpl = [];
+//   for(let i = 0; i < firSpl.length;i++){
+//     let valSpl = firSpl[i].split('=')
+//     secSpl.push([valSpl[0],valSpl[1]])
+//   }
+//   const thiJsonStr = Object.fromEntries(secSpl)
+//   return thiJsonStr;
+// }
+
+
+
+
+function makeQuerystring (values, firstSplit='&', secondSplit='=') {
+
+  const decoding = decodeURIComponent(values)
+  const firSpl = decoding.split(firstSplit)
+  const secSpl = [];
+  for(let i = 0; i < firSpl.length;i++){
+    let valSpl = firSpl[i].split(secondSplit)
+    secSpl.push([valSpl[0],valSpl[1]])
+  }
+  const thiJsonStr = Object.fromEntries(secSpl)
+  return thiJsonStr;
+}
+
+module.exports = makeQuerystring;
