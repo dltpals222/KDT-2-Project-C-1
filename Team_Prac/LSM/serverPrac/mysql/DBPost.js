dc2aea6e0b5290fc428d43614521d8e9aec0ac4d
leesemin <leesemin222@gmail.com>
2023-04-04 18:47:58 +0900

    작동실험 해볼것

---
+++ Team_Prac/LSM/serverPrac/mysql/DBPost.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql')
+const dbconfig = require('./dbconfig.js')
+const mysqlInsertModule = require('../server/mysqlQuerymodule.js')
+
+class saveJSONToDatabase {
+  constructor(bodyStringify){
+    this.bodyStringify = bodyStringify;
+  }
+
+  start(){
+    const database = mysql.createConnection(dbconfig);
+
+    database.connect((err)=> {
+      if(err) {
+        console.error('연결실패',err);
+      } else {
+        console.log(`${dbconfig.port} 연결 성공`);
+      }
+    }); //connet 연결
+
+    //객체화
+    const usersDateParse = JSON.parse(this.bodyStringify);
+    const values = usersDateParse.forEach(value => {value.name,value.phone,value.email});
+    console.log(values);
+
+    //쿼리실행
+    database.query(mysqlInsertModule, values,(err, results)=> {
+      if (err){
+        console.error('쿼리실행 실패',err);
+      } else {
+        console.log('결과물 확인',results);
+      };
+
+      //연결종료
+      connetion.end();
+    })
+  }
+  
+}
+
+module.exports = saveJSONToDatabase;
