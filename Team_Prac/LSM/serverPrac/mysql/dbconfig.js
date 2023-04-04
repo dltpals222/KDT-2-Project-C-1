92bc0277790ccde7e8115bfa1743b5d1f84986f7
leesemin <leesemin222@gmail.com>
2023-04-04 18:47:34 +0900

    mysql 데이터정보

---
+++ Team_Prac/LSM/serverPrac/mysql/dbconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dbconfig : {
+    host : 'localhost',
+    user : 'root',
+    password : 'dltpals123!!',
+    port : '3306',
+    datebase : 'pracnode',
+    connetionLimit : 5
+  }
+}
+
