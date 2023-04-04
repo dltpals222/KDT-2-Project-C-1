505c909c889ab3b70fb8565c697cf6eaaf6cbdf6
leesemin <leesemin222@gmail.com>
2023-04-04 18:50:02 +0900

    export문구를 변경

--- Team_Prac/LSM/serverPrac/modules/tagMakeCall.js
+++ Team_Prac/LSM/serverPrac/modules/tagMakeCall.js
@@ -1,10 +1,26 @@
-module.exports = { 
-  tagMakeCall : function(parentName, tagName, callback) {
+// module.exports = { 
+//   tagMakeCall : function(parentName, tagName, callback) {
+//   let element = document.createElement(tagName);
+//   parentName.appendChild(element);
+//   if(callback){
+//     callback(element);
+//   }
+//   return element;
+// }
+// }
+
+
+
+
+
+function tagMakeCall (parentName, tagName, callback) {
   let element = document.createElement(tagName);
   parentName.appendChild(element);
   if(callback){
     callback(element);
-  }
+  };
   return element;
-}
-}
+};
+
+
+module.exports = tagMakeCall;
