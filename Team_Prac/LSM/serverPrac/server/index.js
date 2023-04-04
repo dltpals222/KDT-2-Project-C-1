ddab392b635136b4594f44b466dcc7a381171229
leesemin <leesemin222@gmail.com>
2023-04-04 18:46:17 +0900

    모듈 ESM방식에서 CommonJS방식으로 변경됐을때 사용되나 보기

--- Team_Prac/LSM/serverPrac/server/index.js
+++ Team_Prac/LSM/serverPrac/server/index.js
@@ -1,8 +1,8 @@
-const tagMakeCall = require('../modules/tagMakeCall')
+const {tagMakeCall} = require('../modules/tagMakeCall.js')
 const classPost = require('./classPost.js')
 
 const root = document.getElementById('root')
-const name = document.getElementById('ip-name')
-const phone = document.getElementById('ip-phone')
-const email = document.getElementById('ip-email')
 
+tagMakeCall(root, 'ul',function(element){
+  element.innerHTML = "ul태그 만들기"
+})
