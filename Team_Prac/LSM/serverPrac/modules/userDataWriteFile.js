37eec59a74789520a25d185d89b7bdfc737fba36
leesemin <leesemin222@gmail.com>
2023-04-10 17:28:18 +0900

    파일을 parse로 읽어와서 그 안의 데이터에 추가데이터를 push한 후에 다시 직렬화하여 파일 수정을 한다. 1차 수정 완

--- Team_Prac/LSM/serverPrac/modules/userDataWriteFile.js
+++ Team_Prac/LSM/serverPrac/modules/userDataWriteFile.js
@@ -1,10 +1,24 @@
-const fs = require('fs')
+const fs = require("fs");
 
-// function userDataWriteFile (value){
-//   fs.writeFile('../JSON/userDate.json',value,(err) => {console.error(err)})
-// }
-function userDataWriteFile (value){
-  fs.appendFile('../JSON/userDate.json',value,(err) => {console.error(err)})
+function userDataWriteFile(value) {
+  fs.readFile("../JSON/userDate.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    const userDateParse = JSON.parse(data);
+    const newUserDate = new value();
+    userDateParse.push(newUserDate);
+
+    fs.writeFile(
+      "../JSON/userDate.json",
+      JSON.stringify(userDateParse),
+      (err) => {
+        if (err) throw err;
+        console.log("데이터가 정상적으로 저장됐습니다.");
+      }
+    );
+  });
 }
+// function userDataWriteFile (value){
+//   fs.appendFile('../JSON/userDate.json',value,(err) => {console.error(err)})
+// }
 
 module.exports = userDataWriteFile;
