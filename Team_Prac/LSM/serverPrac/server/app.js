5a791489c4e668855cf5e4aee8fa558cdc4dd7e7
leesemin <leesemin222@gmail.com>
2023-04-10 18:03:58 +0900

    모듈적용하려고 했는데 모듈이 오류남

--- Team_Prac/LSM/serverPrac/server/app.js
+++ Team_Prac/LSM/serverPrac/server/app.js
@@ -1,53 +1,54 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
-const path = require('path')
-const qs = require('querystring')
-const fsWF = require('../modules/userDataWriteFile')
-const udts = require('../mysql/mysqlStringify') 
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const qs = require("querystring");
+const fsWF = require("../modules/userDataWriteFile");
+// const udts = require('../mysql/mysqlStringify')
 
 class Server {
-  constructor(port){
-    this.port = port
+  constructor(port) {
+    this.port = port;
   }
 
-  start(){
-    http.createServer((req, res) => {
-      const reqUrl = url.parse(req.url,true);
-      const pathName = reqUrl.pathname;
-      const method = req.method;
+  start() {
+    http
+      .createServer((req, res) => {
+        const reqUrl = url.parse(req.url, true);
+        const pathName = reqUrl.pathname;
+        const method = req.method;
 
-      if(pathName === '/' && method === 'GET'){
-        fs.readFile(path.join(__dirname,'index.html'),(err, data) => { 
-          if(err){
-            res.writeHead(500, {'content-type':'text/plain'})
-            res.end('500 서버에 문제가 있습니다.')
-            console.error(err)
-          } else {
-            res.writeHead(200, {'content-type':'text/html'})
-            res.write(data)
-            res.end()
-          }
-        })
-      } else if(pathName === '/' && method === 'POST') {
-        let body = '';
-        req.on('data',chunk => {
-          body += chunk.toString();
-        }) //req.on data끝
-        req.on('end',()=> {
-          console.log(body);
-          let bodyObj = new qs.parse(body);
-          console.log(bodyObj);
-          let bodyStringify = JSON.stringify(bodyObj,null,2);
-          console.log(bodyStringify)
-          fsWF(bodyStringify)
+        if (pathName === "/" && method === "GET") {
+          fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+            if (err) {
+              res.writeHead(500, { "content-type": "text/plain" });
+              res.end("500 서버에 문제가 있습니다.");
+              console.error(err);
+            } else {
+              res.writeHead(200, { "content-type": "text/html" });
+              res.write(data);
+              res.end();
+            }
+          });
+        } else if (pathName === "/" && method === "POST") {
+          let body = "";
+          req.on("data", (chunk) => {
+            body += chunk.toString();
+          }); //req.on data끝
+          req.on("end", () => {
+            console.log(body);
+            let bodyParse = new qs.parse(body);
+            fsWF(bodyParse);
 
-
-        }) //req.on end 끝
-      } //post 끝
-    }).listen(this.port,console.log(`${this.port} 정상작동 합니다.`)) // createserver 끝
+            // console.log(bodyParse);
+            // let bodyStringify = JSON.stringify(bodyParse,null,2);
+            // console.log(bodyStringify)
+            // fsWF(bodyStringify)
+          }); //req.on end 끝
+        } //post 끝
+      })
+      .listen(this.port, console.log(`${this.port} 정상작동 합니다.`)); // createserver 끝
   } //start() 종료
 } //class server 끝
 
 new Server(2080).start();
-
