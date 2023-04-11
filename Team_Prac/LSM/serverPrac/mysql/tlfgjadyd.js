618660202d8f88ec6bea47d99d3ff61e50665f2e
leesemin <leesemin222@gmail.com>
2023-04-11 18:05:10 +0900

    json을 push로 정보를 추가할때 문제점생김

--- Team_Prac/LSM/serverPrac/mysql/tlfgjadyd.js
+++ Team_Prac/LSM/serverPrac/mysql/tlfgjadyd.js
@@ -1,20 +1,49 @@
-import makeQuerystring from "./makeQuerytring";
-import usersDataToStringify from "./mysqlStringify";
-import userDataWriteFile from "../modules/userDataWriteFile";
-import dbpost from "./DBPost";
-import qs from "querystring";
-// const userdataJ = require('../JSON/userDate')
+let a = [];
 
-const aaaaa = qs.parse(
-  "name=%E3%85%81%E3%84%B4%E3%85%87%E3%84%B9&Phone=1274&email=%E3%85%81%E3%84%B4%E3%85%87%E3%84%B9"
-);
-console.log(aaaaa);
-const bbbbb = JSON.stringify(aaaaa, null, 2);
-console.log(bbbbb);
-let database = [];
-database.push(bbbbb);
-userDataWriteFile(bbbbb);
+let user = { name: "lee", age: 1, num: 2 };
 
-console.log(database);
+a.push(user);
 
-// dbpost(userdataJ)
+// console.log(a);
+
+let inAdd = { name: "se", age: 2, num: 12 };
+
+a.push(inAdd);
+
+// console.log(a);
+
+let inAdd2 = { name: "min", age: 3, num: 112 };
+
+a.push(inAdd2);
+
+// console.log(a);
+
+const aStringify = JSON.stringify(a);
+
+// console.log(aStringify);
+
+const bParse = JSON.parse(aStringify);
+
+// console.log(bParse);
+
+let inAdd3 = { name: "leese", age: 5, num: 1221 };
+
+// const c = bParse + inAdd3;
+
+bParse.push(inAdd3);
+
+console.log(bParse);
+
+const cStringify = JSON.stringify(bParse);
+console.log(cStringify);
+
+const cStringifyToParse = JSON.parse(cStringify);
+console.log(cStringifyToParse);
+
+// import fs from "fs";
+
+// fs.readFile("../JSON/userDate.json", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+//   console.log(JSON.parse(data));
+// });
