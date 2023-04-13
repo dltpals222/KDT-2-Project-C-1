68ffcf2a16a2c7de966f4d3501b6dc3cfbd456a1
LeeKyoungtaek <dlrudxor0302@gmail.com>
2023-04-13 15:48:05 +0900

    test

--- Team_Prac/ProtoType-1/common/common/common_css.js
+++ Team_Prac/ProtoType-1/common/common/common_css.js
@@ -41,7 +41,7 @@ flexMaker(search.children[1], "", "", "", "", "30%", "15px");
 flexMaker(search.children[2], "", "", "", "", "10%", "20px");
 search.children[2].style.fontWeight = "bold";
 //main
-flexMaker(main, "flex", "row", "start", "center", "100vw", "200vh");
+flexMaker(main, "flex", "row", "start", "center", "100vw", "75vh");
 //footer
 flexMaker(footer, "flex", "row", "start", "center", "100vw", "20vh");
 footer.style.backgroundColor = "royalblue";
