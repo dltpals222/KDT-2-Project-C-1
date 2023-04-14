3b6d10ca22016fa1567764a96ccf9ebaec75c0c6
leesemin <leesemin222@gmail.com>
2023-04-14 16:09:01 +0900

    살짝 개량함

--- Team_Prac/ProtoType-1/common/recipe_search/module/tagmake.js
+++ Team_Prac/ProtoType-1/common/recipe_search/module/tagmake.js
@@ -1,10 +1,17 @@
-function tagMake(parent, tagName, callback){
-  const element = document.createElement(tagName)
-  if(callback){
-    callback(element)
+function tagMake(parent, tagName, num = 1, callback) {
+  if (num === 1) {
+    const element = document.createElement(tagName);
+    if (callback) {
+      callback(element);
+    }
+    parent.appendChild(element);
+    return element;
+  } else {
+    for (let i = 0; i < num; i++) {
+      const element = document.createElement(tagName);
+      parent.appendChild(element);
+    }
   }
-  parent.appendChild(element)
-  return element
 }
 
-export default tagMake
+export default tagMake;
