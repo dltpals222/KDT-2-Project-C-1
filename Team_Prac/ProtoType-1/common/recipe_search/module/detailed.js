825f94cce1f6012ba12ecfe7216c3453c4159811
leesemin <leesemin222@gmail.com>
2023-04-15 15:31:52 +0900

    이중배열부분 value값은 숫자가 증가하는 식으로 변경

--- Team_Prac/ProtoType-1/common/recipe_search/module/detailed.js
+++ Team_Prac/ProtoType-1/common/recipe_search/module/detailed.js
@@ -11,21 +11,23 @@ function detailedSelectOption(parent, title, option) {
 
   elementDIV.innerText = title;
 
-  //option 작성방법 [[value1,text1],[value2,text2]......]
-  //예시 [['재료','재료'],['hate','기피재료']]
+  //option 작성방법 [text1,text2......]
+  //예시 ['재료','기피재료'.....]
   if (typeof option === "object") {
     for (let i = 0; i < option.length; i++) {
       tagMake(elementSelect, "option", 1, function (element) {
-        element.setAttribute("value", option[i][0]);
-        element.innerText = option[i][1];
+        element.setAttribute("value", `schOpt-${i + 1}`);
+        element.innerText = option[i];
       });
     }
   } else {
-    console.error("option은 이중배열이여야 합니다.");
+    console.error("option은 배열이여야 합니다.");
   }
 
   //style부분
-  styleWHBB(elementA, "36%", "");
+  styleWHBB(elementA, "", "27px", "", "", function (element) {
+    element.margin = "16px";
+  });
   styleDisplay(elementA, "flex", "space-evenly", "center");
   styleWHBB(elementDIV, "40%", "25px", "1px solid black");
   styleWHBB(elementSelect, "60%", "27px", "1px solid black");
