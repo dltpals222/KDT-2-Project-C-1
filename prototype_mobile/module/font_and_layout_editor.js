43868864281d81c1fc093471d79469b45afc595c
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-04-20 16:45:40 +0900

    레이아웃, 폰트 에디터 모듈 주석변경

--- prototype_mobile/module/font_and_layout_editor.js
+++ prototype_mobile/module/font_and_layout_editor.js
@@ -1,6 +1,6 @@
+//border 값을 "1"로주면 "1px solid black"
 // 원하는 폰트 바꾸고 싶을때 직접 객체를 만들 것
 // 객체 만드는 예시
-//border 값을 "1"로주면 "1px solid black"
 // ex)
 // const fontStyle = { fontSize: "30px", color: "white" };
 //함수 호출시 순서 (parent, 넓이, 높이, 외곽선, 배경, 폰트, 콜백)
