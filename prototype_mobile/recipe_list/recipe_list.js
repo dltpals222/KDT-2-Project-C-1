be099f7d0c76616d4b8e7e17ac38c596c883d912
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-04-24 17:47:28 +0900

    태그만들기 중간 저장

--- prototype_mobile/recipe_list/recipe_list.js
+++ prototype_mobile/recipe_list/recipe_list.js
@@ -13,16 +13,26 @@ const {multiAndSingleTagMaker,PositionEditor,fontAndLayoutEditor,kingGodFlexEdit
   // h : //빈칸으로 둘 시 a와 같은 동작을 함
 } */
 const ids = {
-  rootId : ['header','main','footer'],
-  mainId : ['advanced-search','list-wrap','number-list-wrap'],
-  recipeBtnWrap : "recipe-regist",
-  recipeBtn : "recipe-regist-btn",
-  f : {id : 'root'},
+  recipeListTextId : ['recipe-name','recipe-need-ingredient','recipe-need-equipment','recipe-writer','recipe-number-referrals'],
   g : {id : 'root', class : 'object', type : 'module'},
   // h : //빈칸으로 둘 시 a와 같은 동작을 함
 } 
+const root = document.getElementById('root');
 
-multiAndSingleTagMaker(root, 'div', ids.rootId)
-multiAndSingleTagMaker(main, 'div', ids.mainId)
-multiAndSingleTagMaker(main, 'form', ids.recipeBtnWrap)
-multiAndSingleTagMaker(recipeRegist, 'button', ids.recipeBtn)
+const header = multiAndSingleTagMaker(root, 'div', 'header')
+
+const main = multiAndSingleTagMaker(root, 'div', 'main')
+const advancedSearch = multiAndSingleTagMaker(main, 'div', 'advanced-search',1, element=> {
+  element.innerHTML = '상세검색'
+})
+const recipeListWrap = multiAndSingleTagMaker(main, 'div', 'recipe-list-wrap')
+const recipeListBox = multiAndSingleTagMaker(recipeListWrap, 'div', 'recipe-list-box')
+const recipeListImage = multiAndSingleTagMaker(recipeListBox, 'image', 'recipe-list-image')
+const recipeListTextWrap = multiAndSingleTagMaker(recipeListBox, 'div', 'recipe-list-text-wrap')
+const recipeListText = multiAndSingleTagMaker(recipeListTextWrap, 'div', ids.recipeListTextId)
+
+const numberListWrap = multiAndSingleTagMaker(main, 'div', 'number-list-wrap')
+const recipeBtnWrap = multiAndSingleTagMaker(main, 'form', 'recipe-btn-wrap')
+const recipeBtn = multiAndSingleTagMaker(recipeBtnWrap, 'button', 'recipe-btn')
+
+const footer = multiAndSingleTagMaker(root, 'div', 'footer')
