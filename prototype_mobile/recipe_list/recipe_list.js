8db0cff07e62326ad3222d451657336404529403
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-04-25 10:42:06 +0900

    레이아웃 만들기 중간 저장

--- prototype_mobile/recipe_list/recipe_list.js
+++ prototype_mobile/recipe_list/recipe_list.js
@@ -14,7 +14,7 @@ const {multiAndSingleTagMaker,PositionEditor,fontAndLayoutEditor,kingGodFlexEdit
 } */
 const ids = {
   recipeListTextId : ['recipe-name','recipe-need-ingredient','recipe-need-equipment','recipe-writer','recipe-number-referrals'],
-  g : {id : 'root', class : 'object', type : 'module'},
+  number : ['number-1','number-2','number-3','number-4','number-5',],
   // h : //빈칸으로 둘 시 a와 같은 동작을 함
 } 
 const root = document.getElementById('root');
@@ -23,15 +23,27 @@ const header = multiAndSingleTagMaker(root, 'div', 'header')
 const main = multiAndSingleTagMaker(root, 'div', 'main')
 const footer = multiAndSingleTagMaker(root, 'div', 'footer')
 
+//상세 검색 박스
 const advancedSearch = multiAndSingleTagMaker(main, 'div', 'advanced-search',1, element=> {
   element.innerHTML = '상세검색'
 })
+//레시피 리스트 박스
 const recipeListWrap = multiAndSingleTagMaker(main, 'div', 'recipe-list-wrap')
-const numberListWrap = multiAndSingleTagMaker(main, 'div', 'number-list-wrap')
-const recipeBtnWrap = multiAndSingleTagMaker(main, 'form', 'recipe-btn-wrap')
-const recipeBtn = multiAndSingleTagMaker(recipeBtnWrap, 'button', 'recipe-btn')
 const recipeListBox = multiAndSingleTagMaker(recipeListWrap, 'div', 'recipe-list-box')
 const recipeListImage = multiAndSingleTagMaker(recipeListBox, 'image', 'recipe-list-image')
 const recipeListTextWrap = multiAndSingleTagMaker(recipeListBox, 'div', 'recipe-list-text-wrap')
 const recipeListText = multiAndSingleTagMaker(recipeListTextWrap, 'div', ids.recipeListTextId)
+
+//레시피 <<맨앞 <이전 1, 2, 3, 4, 5 다음> 맨뒤>> 
+const numberListWrap = multiAndSingleTagMaker(main, 'div', 'number-list-wrap')
+// 맨앞
+const startNumber = multiAndSingleTagMaker(numberListWrap, 'div', 'start-number')
+// 이전
+const beforeNumber = multiAndSingleTagMaker(numberListWrap, 'div', 'before-number')
+const numberList = multiAndSingleTagMaker(numberListWrap, 'div', '',5,)
+
+//레시피 등록 버튼
+const recipeBtnWrap = multiAndSingleTagMaker(main, 'form', 'recipe-btn-wrap')
+const recipeBtn = multiAndSingleTagMaker(recipeBtnWrap, 'button', 'recipe-btn')
+
 
