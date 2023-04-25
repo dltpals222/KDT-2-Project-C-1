a8d65fe9fea21e154792c76d5a221d4639ba68a9
jin <ellenjin51@gmail.com>
2023-04-25 15:17:30 +0900

    메인부분 수정 및 추가

--- prototype_mobile/recipe_register/register_main_info_pic.js
+++ prototype_mobile/recipe_register/register_main_info_pic.js
@@ -8,9 +8,10 @@ const mainBody = document.getElementById('main');
 //mainBody.style.backgroundColor = 'purple';
 mainBody.style.width = '30vw';
 mainBody.style.height = '65vh';
-mainBody.style.border = '2px solid black';
+//mainBody.style.border = '2px solid black';
+mainBody.style.borderRadius = '10px';
 mainBody.style.display = 'grid';
-mainBody.style.gridTemplateRows = '85px';
+mainBody.style.gridTemplateRows = '80px';
 
 
 ///제목부분 : 레시피 이름
@@ -25,38 +26,68 @@ console.dir(mainBody);
 //mainBody.children[1].style.backgroundColor = 'red';
 //mainBody.children[1].style.flexDirection = 'row';
 
+//재료상세부분과 사진첨부부분 컨테이너
 const container = document.createElement('div');
 container.style.width = '90%';
 container.style.height = '70%';
-container.style.border = '2px solid black';
+//container.style.border = '2px solid black';
 container.style.borderRadius = '10px';
-container.style.padding = '7px';
+container.style.padding = '10px';
+container.style.display = 'flex';
+container.style.flexDirection = 'row';
+container.style.justifyContent = 'space-around';
 mainBody.appendChild(container);
 
 //재료 설명 부분
 const ingredient = document.createElement('div');
-ingredient.style.width = '35%';
-ingredient.style.height = '85%';
-ingredient.style.border = '2px solid black';
+ingredient.style.width = '40%';
+ingredient.style.height = '95%';
+ingredient.style.margin = '0';
+ingredient.style.backgroundColor = '#d9d9d9';
+//ingredient.style.border = '2px solid black';
 ingredient.style.borderRadius = '10px';
 ingredient.style.display = 'flex';
 ingredient.style.flexDirection = 'column';
 ingredient.style.alignItems = 'center';
 container.appendChild(ingredient);
 //재료안의 제목부분
-const ingredientTitle = document.createElement('h3');
+const ingredientTitle = document.createElement('h2');
 ingredientTitle.textContent = '재료';
 ingredient.appendChild(ingredientTitle);
 //재료안의 설명줄부분
+/*
 const ingredientList = document.createTextNode('재료1, 재료2, 재료3, 기타등등....');
 ingredient.appendChild(ingredientList);
 ingredientList.style.textAlign = 'justify';
+*/
+
+/*
+for(let i = 0; i < 3; i++){
+  //ingredientList[i].textContent = "재료";
+  const ingredientList = document.createElement('li');
+  ingredientList.style.listStyleType = 'number';
+  ingredientList.style.border = '2px solid black';
+  ingredient.appendChild(ingredientList[i]);
+}
+*/
+
+//const ingredientList = document.createElement('ul');
+for(let i = 0; i < 7; i++){
+  const ingredientList = document.createElement('li');
+  ingredientList.style.listStyleType = 'square';
+  ingredientList.style.borderBottom = '2px solid black';
+  ingredientList.textContent = '재료';
+  ingredient.appendChild(ingredientList);
+}
 
 //사진부분
 const pic = document.createElement('img');
-pic.style.width = '30%';
-pic.style.height = '25%';
-pic.style.border = '2px solid black';
+pic.style.width = '50%';
+pic.style.height = '95%';
+pic.style.margin = '0';
+pic.style.backgroundColor = '#fffeee';
+//pic.style.border = '2px solid black';
+pic.style.borderRadius = '10px';
 container.appendChild(pic);
 
 /*
--- prototype_mobile/recipe_register/register_main_order_box.js
+++ prototype_mobile/recipe_register/register_main_order_box.js
@@ -2,8 +2,9 @@
 //레시피 순서
 const recipeOrder = document.createElement('div');
 recipeOrder.style.width = '90%';
-recipeOrder.style.height = '50%';
-recipeOrder.style.border = '2px solid black';
+recipeOrder.style.height = '80%';
+recipeOrder.style.backgroundColor = '#d9d9d9';
+//recipeOrder.style.border = '2px solid black';
 recipeOrder.style.borderRadius = '10px';
 recipeOrder.style.padding = '7px';
 mainBody.appendChild(recipeOrder);
@@ -13,10 +14,20 @@ recipeOrder.style.justifyContent = 'space-between';
 //레시피 순서안의 설명
 const info = document.createTextNode('1)요리를 시작한다');
 recipeOrder.appendChild(info);
+/*
+const info = document.createElement('ol');
+for(let i = 0; i < 5; i++){
+  const order = document.createElement('li');
+  order.style.listStyleType = 'number';
+  order.innerText = '요리순서';
+  info.appendChild(order);
+}
+*/
 //레시피 순서안의 사진
 const innerPic = document.createElement('img');
 innerPic.style.width = '40%';
 innerPic.style.height = '95%';
-innerPic.style.border = '2px solid black';
+innerPic.style.backgroundColor = '#fffeee';
+//innerPic.style.border = '2px solid black';
 innerPic.style.borderRadius = '10px';
 recipeOrder.appendChild(innerPic);
