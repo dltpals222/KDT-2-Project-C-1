9b859b55fa631b2f243270d4e37732b6b09cad84
jin <ellenjin51@gmail.com>
2023-04-25 11:15:36 +0900

    레시피 등록부분 수정 및 추가

--- prototype_mobile/recipe_register/register_main.js
+++ prototype_mobile/recipe_register/register_main.js
@@ -4,8 +4,6 @@ import all_mighty_editor from "../module/all_mighty_editor.js";
 const middleBody = multiAndSingleTagMaker('body','div','main',length,callback);
 */
 
-
-
 const mainBody = document.getElementById('main');
 //mainBody.style.backgroundColor = 'purple';
 mainBody.style.width = '30vw';
@@ -36,15 +34,27 @@ pic.style.height = '30%';
 pic.style.border = '2px solid black';
 mainBody.appendChild(pic);
 
-const recipeOrder = document.createElement('div');
-recipeOrder.style.width
 
+//레시피 순서
+const recipeOrder = document.createElement('div');
+recipeOrder.style.width = '90%';
+recipeOrder.style.height = '30%';
+recipeOrder.style.border = '2px solid black';
+mainBody.appendChild(recipeOrder);
+//레시피 순서안의 설명
+const info = document.createTextNode('요리를 시작한다');
+recipeOrder.appendChild(info);
+//레시피 순서안의 사진
+const innerPic = document.createElement('img');
+innerPic.style.width = '40%';
+innerPic.style.height = '50%';
+innerPic.style.border = '2px solid black';
+recipeOrder.appendChild(innerPic);
 
 /*
 const line = document.querySelector('hr');
 line.style.border = '5px solid black';
 */
-
 
 /*
 const main1  = document.createElement('div');
