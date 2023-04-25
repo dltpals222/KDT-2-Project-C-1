a69692b50c0b68f76ccf1b947b3c8796a82bb040
leesemin <leesemin222@gmail.com>
2023-04-25 15:38:33 +0900

    게시판 페이지넘기기

---
+++ prototype_mobile/boardPage/paging.js
@@ -0,0 +1,48 @@
+import all_mighty_editor from '../module/all_mighty_editor.js'
+
+const {multiAndSingleTagMaker,PositionEditor,fontAndLayoutEditor,kingGodFlexEditor,allMightyStyleEditor} = all_mighty_editor
+
+const root = document.getElementById('root')
+const GClength = 10
+const ids = {
+  mainList : ["", 'div','board-list'],
+  mainPage : ["", 'div','board-paging'],
+  mainListChild : ["", 'div',"",5],
+  mainPageChild : {
+    first : ["", 'div','page-down'],
+    second : ["", 'div','current-page'],
+    third : ["", 'div','page-up'],
+  },
+  mainPageGrandChild : {
+    first : [["",'div',"pd-home",1,element => {
+      element.innerText = '처음'
+    }],["",'div',"pd-prev",1,element => {
+      element.innerText = '이전'
+    }]],
+    second : ["", 'div'],
+    third : [["", 'div','pu-next',1,element => {
+      element.innerText = '다음'
+    }],["", 'div','pu-end',1,element => {
+      element.innerText = '마지막'
+    }]]
+  }
+}
+
+
+const boardList = multiAndSingleTagMaker(root,ids.mainList[1],ids.mainList[2])
+const boardPaging = multiAndSingleTagMaker(root,ids.mainPage[1],ids.mainPage[2])
+multiAndSingleTagMaker(boardList,ids.mainListChild[1],ids.mainListChild[2])
+const pageDown = multiAndSingleTagMaker(boardPaging,ids.mainPageChild.first[1],ids.mainPageChild.first[2])
+const currentPage = multiAndSingleTagMaker(boardPaging,ids.mainPageChild.second[1],ids.mainPageChild.second[2])
+const pageUp = multiAndSingleTagMaker(boardPaging,ids.mainPageChild.third[1],ids.mainPageChild.third[2])
+
+const pdHome = multiAndSingleTagMaker(pageDown,ids.mainPageGrandChild.first[0][1],ids.mainPageGrandChild.first[0][2])
+const pdPrev = multiAndSingleTagMaker(pageDown,ids.mainPageGrandChild.first[1][1],ids.mainPageGrandChild.first[1][2])
+for(let i =0; i< GClength;i++){
+  multiAndSingleTagMaker(currentPage,ids.mainPageGrandChild.second[1],"",1,element => {element.innerText = i+1})
+}
+const puNext = multiAndSingleTagMaker(pageUp,ids.mainPageGrandChild.third[0][1],ids.mainPageGrandChild.third[0][2])
+const puEnd = multiAndSingleTagMaker(pageUp,ids.mainPageGrandChild.third[1][1],ids.mainPageGrandChild.third[1][2])
+
+kingGodFlexEditor(root,'row','center','center')
+fontAndLayoutEditor(boardPaging,'80vw','10vh','1')
