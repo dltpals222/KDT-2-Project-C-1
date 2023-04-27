5cc0fed3b98ac39cf48f92003cee06b843d86bef
leesemin <leesemin222@gmail.com>
2023-04-27 15:00:12 +0900

    페이지네이션 작업 중

--- prototype_mobile/boardPage/paging.js
+++ prototype_mobile/boardPage/paging.js
@@ -31,22 +31,38 @@ const ids = {
 const numberStyle = ['15px','10px',"1"]
 const tagASet={href : '#'}
 
-
+//게시글이 나타날 div
 const boardList = multiAndSingleTagMaker(root,ids.mainList[1],ids.mainList[2])
-const numberListWrap = multiAndSingleTagMaker(root,ids.mainPage[1],ids.mainPage[2])
+//게시글 임시 div 추가
 multiAndSingleTagMaker(boardList,ids.mainListChild[1],ids.mainListChild[2],ids.mainListChild[3],ids.mainListChild[4])
+
+//페이지네이션 최상위 div
+const numberListWrap = multiAndSingleTagMaker(root,ids.mainPage[1],ids.mainPage[2])
+//이전 페이지 상위 div
 const pageDown = multiAndSingleTagMaker(numberListWrap,ids.mainPageChild.first[1],ids.mainPageChild.first[2])
+//현재 페이지 나타내는 구간
 const currentPage = multiAndSingleTagMaker(numberListWrap,ids.mainPageChild.second[1],ids.mainPageChild.second[2])
+//다음 페이지 상위 div
 const pageUp = multiAndSingleTagMaker(numberListWrap,ids.mainPageChild.third[1],ids.mainPageChild.third[2])
 
+//맨앞 버튼
 const startNumber = multiAndSingleTagMaker(pageDown,ids.mainPageGrandChild.first[0][1],ids.mainPageGrandChild.first[0][2],ids.mainPageGrandChild.first[0][3],ids.mainPageGrandChild.first[0][4])
+
+//이전 버튼
 const beforeNumber = multiAndSingleTagMaker(pageDown,ids.mainPageGrandChild.first[1][1],ids.mainPageGrandChild.first[1][2],ids.mainPageGrandChild.first[1][3],ids.mainPageGrandChild.first[1][4])
+
+//현재 페이지 버튼
 for(let i =0; i< GClength;i++){
   multiAndSingleTagMaker(currentPage,ids.mainPageGrandChild.second[1],ids.mainPageGrandChild.second[2],ids.mainPageGrandChild.second[3],element => {element.setAttribute('id',`number-${i+1}`), element.innerText = i+1})
 }
+
+//다음 버튼
 const nextNumber = multiAndSingleTagMaker(pageUp,ids.mainPageGrandChild.third[0][1],ids.mainPageGrandChild.third[0][2],ids.mainPageGrandChild.third[0][3],ids.mainPageGrandChild.third[0][4])
+
+//맨뒤 버튼
 const endNumber = multiAndSingleTagMaker(pageUp,ids.mainPageGrandChild.third[1][1],ids.mainPageGrandChild.third[1][2],ids.mainPageGrandChild.third[1][3],ids.mainPageGrandChild.third[1][4])
 
+//css구간
 kingGodFlexEditor(root,'row','center','center')
 for(let i = 0;i<root.children.length;i++){
   kingGodFlexEditor(root.children[i],'','center','center')
@@ -62,6 +78,8 @@ fontAndLayoutEditor(numberListWrap,'80vw','3vh','1')
 fontAndLayoutEditor(currentPage,'35%','','1')
 kingGodFlexEditor(currentPage,'','center','space-evenly')
 
+
+
 const pagination = {
   totalPaper : 462,   //게시물 전체 갯수
   onePageData : 20,   //한페이지에 나타낼 데이터 수
@@ -73,25 +91,24 @@ const pagination = {
 const totalPage = Math.ceil(pagination.totalPaper/pagination.onePageData);
 
 //화면에 보여질 페이지 그룹
-const aPage = Math.ceil(pagination.currentPage/pagination.onePageNumber)
+const aPageGroup = Math.ceil(pagination.currentPage/pagination.onePageNumber)
+
+//화면에 보여질 페이지 그룹 끝부분
+const EndPageGroup = Math.ceil(totalPage/pagination.onePageNumber)
 
 //화면에 그려질 첫번째 페이지
 let displayOnePage = 0;
 if(totalPage - (pagination.onePageNumber -1) < 0){
-  displayOnePage = 1;
+  displayOnePage = 1; //계산된 값이 0 이하면 첫번째 페이지이다.
 } else {
   displayOnePage = totalPage - (pagination.onePageNumber -1);
 }
 
 //화면에 그려질 마지막 페이지
 let displayEndPage = 0;
-if((aPage * pagination.onePageNumber) > totalPage){
-  displayEndPage = totalPage;
+if((aPageGroup * pagination.onePageNumber) > totalPage){
+  displayEndPage = totalPage; //계산된 값이 총페이지보다 많으면 마지막 페이지이다.
 } else {
-  displayEndPage = aPage * pagination.onePageNumber;
+  displayEndPage = aPageGroup * pagination.onePageNumber;
 }
 
-
-
-console.log(totalPage)
-console.log(MathCeil)
