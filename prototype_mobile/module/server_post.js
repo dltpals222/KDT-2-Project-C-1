48f35f6bbeac87d6a0853782340bae1245ccd8dd
LeeKyoungtaek <dlrudxor0302@gmail.com>
2023-05-02 16:30:38 +0900

    post test중 팀장님 호출

--- prototype_mobile/module/server_post.js
+++ prototype_mobile/module/server_post.js
@@ -1,84 +1,32 @@
-import fs from 'fs'
 import qs from 'querystring'
-import http from 'http'
-import mysql from 'mysql'
-import url from 'url'
+import serverReadFileModule from './server_readfile.js'
+import dbConfig from '../mysql/mysql_connect.js'
 
-const server = http.createServer((req, res) => {
-  const parseUrl = url.parse(req.url);
-  const pathName = parseUrl.pathname;
-  const Method = req.method;
+function a (chunk) {
+  let body = '';
+  body += chunk;
 
-  if (Method === 'GET' && pathName === '/') {
-    serverReadFileModule(res, '../mysql/mysql_input.html', 'text/html', 200);
-  } else if (Method === 'POST' && pathName === '/set') {
-    serverPostModule(req, res);
-  }
-})
+  let post = qs.parse(body);
+  let name = post.name;
+  let type = post.type;
+  let taek = post.taek;
 
-server.listen(2080, function (err) {
-  if (err) {
-    console.error("서버 안돌아감");
-  } else {
-    console.log("서버 돌아감");
-  }
-})
+  let array = [];
+  array.push(name, type, parseInt(taek, 10));
+  console.log(array);
 
-function serverReadFileModule(res, path, contentTypeValue, statusCode) {
-  fs.readFile(path, (err, data) => {
+  const query = 'insert into a (name, type, taek) values (?,?,?)';
+  dbConfig.connect();
+  dbConfig.query(query, array, (err) => {
     if (err) {
-      res.writeHead(500, { 'content-type': 'text/html' });
-      res.write('500 internet error');
-      res.end();
+      console.error('쿼리실행 실패', err);
     } else {
-      res.writeHead(statusCode, { 'content-type': contentTypeValue });
-      res.write(data);
-      res.end();
+      console.log("쿼리실행성공");
     }
-  }) //readFile 끝
-}
-
-function serverPostModule(req, res) {
-
-  req.on('data', function (chunk) {
-    let body = '';
-    body += chunk;
-
-    const dbConfig = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'kids0302',
-      database: 'taek',
-      port: 3306, //Mysql 포트
-      connectionLimit: 5, //동시에 db에 접속가능한 클라이언트 수
-      waitForConnections: true //클라이언트가 연결을 요청할 때 대기 여부
-    })
-
-    dbConfig.connect();
-    console.log(body);
-    let post = qs.parse(body);
-    console.log(post)
-    let name = post.name;
-    let type = post.type;
-    let taek = post.taek;
-    console.log(name);
-
-    let array = [];
-    array.push(name, type, parseInt(taek, 10));
-    console.log(array);
-    const query = 'insert into a (name, type, taek) values (?,?,?)';
-
-    dbConfig.query(query, array, (err) => {
-      if (err) {
-        console.error('쿼리실행 실패', err);
-      } else {
-        console.log("쿼리실행성공");
-      }
-    })
-    dbConfig.end();
-  });
-
-  req.on('end', function () {
-    serverReadFileModule(res,'../mysql/test.html', 'text/html', 200)
   })
+  dbConfig.end();
+
+
 }
+
+export default a;
