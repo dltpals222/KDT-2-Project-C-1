ea49aab5a7f140671cea08124c89d1d575f8f2a1
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-05-02 17:51:27 +0900

    mysql database 이름   create table add_recipe{   id int NOT NULL AUTO_INCREMENT;   title varchar(255) NOT NULL;   ingredients varchar(255) NOT NULL;   content varchar(255) NOT NULL;   primary key(id)}

--- prototype_mobile/mysql/mysql_test.js
+++ prototype_mobile/mysql/mysql_test.js
@@ -16,11 +16,12 @@ const db = mysql.createConnection({
 /* 
 
   mysql database 이름 
+  create table add_recipe{
   id int NOT NULL AUTO_INCREMENT;
   title varchar(255) NOT NULL;
   ingredients varchar(255) NOT NULL;
   content varchar(255) NOT NULL;
-  primary key(id);
+  primary key(id)}
 */
 db.connect();
 const newDbArray = [];
