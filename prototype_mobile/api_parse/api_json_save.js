b50daaea68b9479b00ad2b611352245e46a329a0
ParkDalJae <dv.over931@gmail.com>
2023-05-03 16:59:45 +0900

    1번. 고정 주소 기준 파일저장 확인

---
+++ prototype_mobile/api_parse/api_json_save.js
@@ -0,0 +1,31 @@
+import http from "http";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const apiUrl =
+  "http://openapi.foodsafetykorea.go.kr/api/ea64844179af470daeb3/COOKRCP01/json/1/5";
+const fileName = "data.json";
+const filePath = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  fileName
+);
+
+http
+  .get(apiUrl, (res) => {
+    let rawData = "";
+    res.on("data", (chunk) => {
+      rawData += chunk;
+    });
+    res.on("end", async () => {
+      try {
+        await fs.writeFile(filePath, rawData);
+        console.log(`Data saved to ${fileName}`);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  })
+  .on("error", (error) => {
+    console.error(error);
+  });
