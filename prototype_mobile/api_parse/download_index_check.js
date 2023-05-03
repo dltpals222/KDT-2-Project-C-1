ef58898db6e309581bc7feff9c3f59ff92028f60
ParkDalJae <dv.over931@gmail.com>
2023-05-03 18:09:59 +0900

    json 파일의 객체 확인용

--- prototype_mobile/api_parse/download_index_check.js
+++ prototype_mobile/api_parse/download_index_check.js
@@ -1,12 +1,8 @@
-const fs = require("fs");
+import fs from "fs";
 
-// JSON 파일 읽기
-const rawData = fs.readFileSync("data.json");
-const jsonData = JSON.parse(rawData);
+const data = fs.readFileSync("./data_2.json", "utf8");
+const recipes = JSON.parse(data);
+const recipeCount = recipes.length;
 
-// 레시피 수 계산
-const recipeCount = Object.keys(jsonData).length;
-
-console.log(
-  `data.json 파일에는 ${recipeCount}개의 레시피가 포함되어 있습니다.`
-);
+// console.log(`총 ${recipeCount}개의 레시피가 있습니다.`);
+console.log(recipes);
