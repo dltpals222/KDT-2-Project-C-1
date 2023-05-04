0d033b9550532ef2f69824ba43583596fb37db96
LeeKyoungtaek <dlrudxor0302@gmail.com>
2023-05-04 10:55:40 +0900

    테스트완성

--- prototype_mobile/module/LKT_test_app.js
+++ prototype_mobile/module/LKT_test_app.js
@@ -10,13 +10,13 @@ const server = http.createServer((req, res) => {
   const Method = req.method;
 
   if (Method === 'GET' && pathName === '/') {
-    serverReadFileModule(res, '../mysql/mysql_input.html', 'text/html', 200);
+    serverReadFileModule(res, '../mysql/LKT_mysql_input.html', 'text/html', 200);
   } else if (Method === 'POST' && pathName === '/set') {
     req.on('data', function(chunk) {
       reqOnData(chunk, 'insert into b (name, type, taek) values (?,?,?)');
     });
     req.on('end', function () {
-      serverReadFileModule(res, '../mysql/test.html', 'text/html', 200)
+      serverReadFileModule(res, '../mysql/LKT_test.html', 'text/html', 200)
     })
   }
 })
