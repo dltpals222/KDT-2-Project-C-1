ef1566d75f74cd54d1b9b6600af02b85e71a4f1e
LeeKyoungtaek <dlrudxor0302@gmail.com>
2023-05-09 16:52:18 +0900

    module 의 post 방식 수정 및 db객체 재사용 해결

--- prototype_mobile/CRUD/LKT_app.js
+++ prototype_mobile/CRUD/LKT_app.js
@@ -7,7 +7,15 @@ import mysql from 'mysql'
 
 const server = http.createServer((req, res) => {
 
-  // res.setHeader('Access-Control-Allow-Origin', '*');
+  const dbSet = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'kids0302',
+    database: 'taek',
+    port: 3306, //Mysql 포트
+    connectionLimit: 5, //동시에 db에 접속가능한 클라이언트 수
+    waitForConnections: true //클라이언트가 연결을 요청할 때 대기 여부
+  })
 
   const parseUrl = url.parse(req.url);
   const pathName = parseUrl.pathname;
@@ -15,14 +23,13 @@ const server = http.createServer((req, res) => {
 
   if (Method === 'GET') {
     switch (pathName) {
+      // json 데이터들과 c,r,u,d 버튼이 있는 페이지
       case "/":
         serverReadFileModule(res, 'LKT_main.html', 'text/html', 200);
         break;
+      // 여기서부터 favicon까지 위의 LKT_main.html을 브라우저에 표출하기 위한 get방식
       case "/LKT_main.js":
         serverReadFileModule(res, 'LKT_main.js', 'text/javascript', 200);
-        break;
-      case "/c":
-        serverReadFileModule(res, 'LKT_R.html', 'text/html', 200);
         break;
       case "/module/all_mighty_editor.js":
         serverReadFileModule(res, '../module/all_mighty_editor.js', 'text/javascript', 200);
@@ -31,8 +38,13 @@ const server = http.createServer((req, res) => {
         serverReadFileModule(res, 'db.json', 'application/json', 200);
         break;
       case '/favicon.ico':
-        (err) =>{if (err) { throw err }}
+        (err) => { if (err) { throw err } }
         break;
+      // c버튼을 눌렀을 때 LKT_R.html을 get방식으로 받아오는 case
+      case "/c":
+        serverReadFileModule(res, 'LKT_R.html', 'text/html', 200);
+        break;
+      // 필요한 파일들을 받아오기 위한 기능
       default:
         console.log(pathName);
     }
@@ -40,25 +52,34 @@ const server = http.createServer((req, res) => {
 
   else if (Method === 'POST') {
     switch (pathName) {
+      // 버튼의 C로직
       case "/set":
         req.on('data', function (chunk) {
-          reqOnData(chunk, 'insert into b (name, type, taek) values (?,?,?)', (dbSet) => {
-            dbSet.query("SELECT * FROM  b", function (err, results) {
-              if (err) {
-                console.error(err)
-              } else {
-                fs.writeFileSync("db.json", JSON.stringify(results, null, 2));
-              }
-            });
-            dbSet.end();
+          const a = reqOnData(chunk);
+          dbSet.connect();
+          dbSet.query('insert into b (name, type, taek) values (?,?,?)', a, (err) => {
+            if (err) {
+              console.error("쿼리실행 실패", err);
+            } else {
+              console.log("쿼리실행성공");
+            }
           });
-        });
+          dbSet.query("SELECT * FROM  b", function (err, results) {
+            if (err) {
+              console.error(err)
+            } else {
+              fs.writeFileSync("db.json", JSON.stringify(results, null, 2));
+            }
+          });
+          dbSet.end();
+        })
         req.on('end', function () {
-          // serverReadFileModule(res, 'LKT_main.html', 'text/html', 200)
           res.writeHead(302, { Location: '/' });
           res.end();
         })
         break;
+
+
     }
   }
 })
