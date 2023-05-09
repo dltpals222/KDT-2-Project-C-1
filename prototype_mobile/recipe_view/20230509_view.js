2aeb32922e78494c6036f6ea07f1f75f82fa4d5b
jin <ellenjin51@gmail.com>
2023-05-09 15:46:16 +0900

    레시피 뷰 addeventlistner 추가

--- prototype_mobile/recipe_view/20230509_view.js
+++ prototype_mobile/recipe_view/20230509_view.js
@@ -26,6 +26,23 @@ const mainPic = multiAndSingleTagMaker(main,'img','mainPic',1,(ele)=>{
 });
 fontAndLayoutEditor(mainPic, '65%', '20%', 1 , 'aqua');
 main.appendChild(mainPic);
+mainPic.style.cursor = 'pointer';
+mainPic.addEventListener('click',function browseImgMainFile(){
+  const formData = new FormData();
+  formData.append('image', imageInput.files[0]);
+
+  fetch('/upload', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -66,6 +83,23 @@ main.appendChild(orderList);
 const orderPic = multiAndSingleTagMaker(orderList , 'img', 'orderPic', 1);
 fontAndLayoutEditor(orderPic, '40%','90%',1, 'skyblue');
 orderList.appendChild(orderPic);
+orderList.style.cursor = 'pointer';
+orderList.addEventListener('click',function browseImgMainFile(){
+  const formData = new FormData();
+  formData.append('image', imageInput.files[0]);
+
+  fetch('/upload', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 ///조리순서안의 조리방법 또는 내용
 const orderInfo = multiAndSingleTagMaker(orderList, 'div', 'orderInfo', 1, (ele)=>{
   /*
--- prototype_mobile/recipe_view/view_main_server.js
+++ prototype_mobile/recipe_view/view_main_server.js
@@ -1,3 +1,4 @@
+/*
 const require = new XMLHttpRequest();
 require.open('GET',);
 require.onreadystatechange = function(){
@@ -14,3 +15,32 @@ require.onreadystatechange = function(){
   }
 }
 require.send();
+*/
+
+const http = require('http');
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/upload') {
+    upload.single('image')(req, res, (err) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('Error uploading file');
+      } else {
+        console.log(req.file);
+        res.statusCode = 200;
+        res.end('File uploaded successfully');
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
