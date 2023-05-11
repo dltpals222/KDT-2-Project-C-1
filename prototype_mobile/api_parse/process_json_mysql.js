0a24dfdd0b5e61e23f3b6f9e6ad6e2119f90e8ca
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-05-11 15:02:20 +0900

    저장

--- prototype_mobile/api_parse/process_json_mysql.js
+++ prototype_mobile/api_parse/process_json_mysql.js
@@ -2,8 +2,6 @@ import mysql from "mysql";
 import fs from "fs";
 import path from "path";
 
-// npm install -s mysql 모듈 설치 해야함
-// 이부분 보통 모듈화 해서 쓰는 것 같음
 // db 연동하기
 const db = mysql.createConnection({
   // 본인 아이디 패스워드 db이름 사용할 것
@@ -23,17 +21,18 @@ fs.readFile(filePath, "utf-8", (err, data) => {
 
   // 데이터 파싱
   const jsonData = JSON.parse(data);
-  const row = jsonData.row;
+  const rows = jsonData.row;
+  console.log(rows);
 
   //테이블 데이터 전체 삭제
-  // db.query('DELETE FROM text WHERE id<10000');
+  // db.query("DELETE FROM parse_data WHERE id<10000");
 
   //데이터 추가
   const sql = "INSERT INTO parse_data (ingredients) VALUES ?";
   //update 시도
   // const sqlUpdate = 'UPDATE text SET (id, name, email) WHERE id>10 ?';
-  const values = row.map((value) => [value.ingredients]);
-  console.log(values);
+  const values = rows.map((value) => value.ingredients);
+  // console.log(values);
 
   db.query(sql, [values], (err, result) => {
     if (err) {
@@ -41,6 +40,6 @@ fs.readFile(filePath, "utf-8", (err, data) => {
     } else {
       console.log("성공");
     }
+    db.end();
   });
-  db.end();
 });
