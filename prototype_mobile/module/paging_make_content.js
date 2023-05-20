c68c7a56fcd5321453c3cea3badbb00e4e8b53d7
leesemin <leesemin222@gmail.com>
2023-05-20 18:39:52 +0900

    make_content.js 주석 설명 완료

--- prototype_mobile/module/paging_make_content.js
+++ prototype_mobile/module/paging_make_content.js
@@ -1,4 +1,28 @@
 //게시글 예제
+
+/**
+ * * @param {object} object - 객체에는 총 5개의 키값이 있어야 한다. 타이틀, 필요재료, 작성자, 추천수, 조회수
+ * * @param {number} i - i 그대로 적으면 된다.
+ * * @return {string} - 문자열로 나옴
+ * 
+ * >레시피 이름 : ${jsonDataTitle[i - 1]} <br> \
+ * 필요 재료 : ${jsonDataIngredients[i - 1]} <br> \
+ * 작성자 : ${jsonDataRegister[i - 1]}<br> \
+ * 추천수 : ${jsonDataRecommend[i - 1]}<br> \
+ * 조회수 : ${jsonDataViews[i - 1]}<br> 
+ * 
+ * * 다음은 객체에 대한 예시입니다.
+ * 
+ *  >const HTMLObject = {\
+ *  jsonDataTitle : jsonDataTitle,\
+ *  jsonDataIngredients : jsonDataIngredients,\
+ *  jsonDataRegister : jsonDataRegister,\
+ *  jsonDataRecommend : jsonDataRecommend,\
+ *  jsonDataViews : jsonDataViews\
+ *  }
+ * 
+ */
+
 export const makeContent = (object, i) => {
   const {jsonDataTitle,jsonDataIngredients,jsonDataRegister,jsonDataRecommend,jsonDataViews} = object
 
@@ -9,15 +33,6 @@ export const makeContent = (object, i) => {
   조회수 : ${jsonDataViews[i - 1]}<br>`
   
 };
-// export const makeContent = (element, i) => {
-//   element.innerHTML = `레시피 이름 : ${jsonDataTitle[i - 1]} <br> 
-//   필요 재료 : ${jsonDataIngredients[i - 1]} <br> 
-//   작성자 : ${jsonDataRegister[i - 1]}<br> 
-//   추천수 : ${jsonDataRecommend[i - 1]}<br> 
-//   조회수 : ${jsonDataViews[i - 1]}<br>`;
-// };
-
-
 
 //레시피 리스트 이미지 스타일
 export const recipeListImage = {
