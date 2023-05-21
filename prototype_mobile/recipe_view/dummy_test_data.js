e017cc63393d8b78558199ad7db546101bf96503
jin <ellenjin51@gmail.com>
2023-05-21 16:56:41 +0900

    view부분 종합적으로 수정 및 정리

---
+++ prototype_mobile/recipe_view/dummy_test_data.js
@@ -0,0 +1,74 @@
+///사용재료 데이터
+const originalIngredients = {
+  ingredients_id : 1,
+  ingredients : 'beef broth'
+};
+console.log(originalIngredients);
+/////////////////////////////////////////////////////////////////
+
+///레시피 조리재료 데이터
+const recipeViewIngredient = {
+  recipe_ingredients_id : '',
+	recipe_id : 1,
+	recipe_weight : '500g',
+	regist_ingredients : 'anything',
+	ingredients_id : '1'
+};
+console.log(recipeViewIngredient);
+console.log(recipeViewIngredient.recipe_weight);
+/////////////////////////////////////////////////////////////////
+
+///레시피 뷰 임의 데이터 ==> 레시피 사진, 레시피 이름, 등록자 정보 및 조회수, 재료
+const recipeViewRegisterInfo = {
+  recipe_id: 1,
+  recipe_register: "김첨지",
+  recipe_title: "설렁탕",
+  thumbnail_img:
+  "http://www.foodsafetykorea.go.kr/uploadimg/cook/20_00028_2.png",
+  recipe_views: 0,
+  recipe_recommend: 0,
+};
+console.log(recipeViewRegisterInfo);
+
+
+///레시피 뷰 임의 데이터 => 조리기구 데이터
+const recipeViewCooker = {
+  cooker_id : 1,
+  cooker : 'pot'
+}
+console.log(recipeViewCooker);
+
+///레시피 조리순서 데이터
+const recipeViewStep = [
+  {
+    step_id: 1,
+    recipe_id: 1,
+    recipe_step_content: "요리를 시작한다.",
+    recipe_step_img: "https://recipe1.ezmember.co.kr/cache/recipe/2018/01/05/3bb9dfbf636d5dd2397b31b8f00700fd1.jpg",
+    recipe_step_number: 3,
+  },
+  {
+    step_id: 2,
+    recipe_id: 1,
+    recipe_step_content: "조리기구를 쓴다.",
+    recipe_step_img: "https://recipe1.ezmember.co.kr/cache/recipe/2023/05/03/e409cd940d27db7c89c4f3c2224299511.jpg",
+    recipe_step_number: 1,
+  },
+  {
+    step_id: 3,
+    recipe_id: 1,
+    recipe_step_content: "불을 킨다.",
+    recipe_step_img: "https://recipe1.ezmember.co.kr/cache/recipe/2023/05/09/e83886d167d4bc7725161451a49a9e161.jpg",
+    recipe_step_number: 2,
+  },
+];
+console.log(recipeViewStep);
+console.dir(recipeViewStep[0].recipe_step_img);
+
+export default {
+  originalIngredients,
+  recipeViewIngredient,
+  recipeViewRegisterInfo,
+  recipeViewCooker,
+  recipeViewStep
+};
---
+++ prototype_mobile/recipe_view/view_app.js
@@ -0,0 +1,95 @@
+import http from 'http'
+import url from 'url'
+import serverReadFileModule from '../module/server_readfile.js'
+import reqOnData from '../module/server_post_copy.js'
+import fs from 'fs';
+import mysql from 'mysql'
+
+const server = http.createServer((req, res) => {
+
+  const parseUrl = url.parse(req.url);
+  const pathName = parseUrl.pathname;
+  const Method = req.method;
+
+  const dbSet = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'kids0302',
+    database: 'taek',
+    port: 3306, //Mysql 포트
+    connectionLimit: 5, //동시에 db에 접속가능한 클라이언트 수
+    waitForConnections: true //클라이언트가 연결을 요청할 때 대기 여부
+  })
+  
+  if (Method === 'GET') {
+    switch (pathName) {
+      // json 데이터들과 c,r,u,d 버튼이 있는 페이지
+      case "/":
+        serverReadFileModule(res, 'LKT_main.html', 'text/html', 200);
+        break;
+      // 여기서부터 favicon까지 위의 LKT_main.html을 브라우저에 표출하기 위한 get방식
+      case "/LKT_main.js":
+        serverReadFileModule(res, 'LKT_main.js', 'text/javascript', 200);
+        break;
+      case "/module/all_mighty_editor.js":
+        serverReadFileModule(res, '../module/all_mighty_editor.js', 'text/javascript', 200);
+        break;
+      case "/db.json":
+        serverReadFileModule(res, 'db.json', 'application/json', 200);
+        break;
+      case '/favicon.ico':
+        (err) => { if (err) { throw err } }
+        break;
+      // c버튼을 눌렀을 때 LKT_R.html을 get방식으로 받아오는 case
+      case "/c":
+        serverReadFileModule(res, 'LKT_R.html', 'text/html', 200);
+        break;
+      case '/r':
+        serverReadFileModule(res, 'LKT_main.html','text/html', 200);
+        break
+      // 필요한 파일들을 받아오기 위한 기능
+      default:
+        console.log(pathName);
+    }
+  }
+
+  else if (Method === 'POST') {
+    switch (pathName) {
+      // 버튼의 C로직
+      case "/set":
+        req.on('data', function (chunk) {
+          const a = reqOnData(chunk);
+          dbSet.connect();
+          dbSet.query('insert into b (name, type, taek) values (?,?,?)', a, (err) => {
+            if (err) {
+              console.error("쿼리실행 실패", err);
+            } else {
+              console.log("쿼리실행성공");
+            }
+          });
+          dbSet.query("SELECT * FROM  b", function (err, results) {
+            if (err) {
+              console.error(err)
+            } else {
+              fs.writeFileSync("db.json", JSON.stringify(results, null, 2));
+            }
+          });
+          dbSet.end();
+        })
+        req.on('end', function () {
+          res.writeHead(302, { Location: '/' });
+          res.end();
+        })
+        break;
+
+    }
+  }
+})
+
+server.listen(2080, function (err) {
+  if (err) {
+    console.error("서버 안돌아감");
+  } else {
+    console.log("서버 돌아감");
+  }
+})
