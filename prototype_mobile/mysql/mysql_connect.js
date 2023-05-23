6481af1d20ab557bfbefbe6b0fe4f995eb04cce3
leesemin <leesemin222@gmail.com>
2023-05-23 23:10:09 +0900

    search작업 일시중지 후 rebase 실행을 위한 커밋

--- prototype_mobile/mysql/mysql_connect.js
+++ prototype_mobile/mysql/mysql_connect.js
@@ -3,9 +3,9 @@ import mysql from "mysql";
 const dbSet = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "admin1234",
+  password: "1q2w3e4r",
   database: "msmg",
-  port: 3306, //Mysql 포트
+  port: 3307, //Mysql 포트
   connectionLimit: 5, //동시에 db에 접속가능한 클라이언트 수
   waitForConnections: true, //클라이언트가 연결을 요청할 때 대기 여부
 });
