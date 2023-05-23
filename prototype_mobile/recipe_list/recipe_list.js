deea238a41b07abd631d7067681e0e5c0e6e621d
ParkDalJae <dv.over931@gmail.com>
2023-05-23 18:31:33 +0900

    issue56 이슈 병합. recipe_list_paging.js 내용 recipelist.js에 합침. recipe_list_paging.js 주석처리

--- prototype_mobile/recipe_list/recipe_list.js
+++ prototype_mobile/recipe_list/recipe_list.js
@@ -1,3 +1,4 @@
+import renderButtonContainer from "../module/paging_render_button.js";
 import all_mighty_editor from "../module/all_mighty_editor.js";
 
 const {
@@ -86,3 +87,37 @@ const recipeBtnStyle = {
   margin: "2%",
 };
 allMightyStyleEditor(recipeBtn, recipeBtnStyle);
+
+//ajax 를 사용해서 xml을 이용한 정보 교환
+const http = new XMLHttpRequest();
+http.onreadystatechange = function () {
+  if (http.readyState === 4 && http.status === 200) {
+    //json 파일에 있는 데이터의 값을 출력
+    const data = JSON.parse(http.responseText);
+    const dataAll = {
+      jsonDataId: data.map((value) => [value.recipe_id]),
+      jsonDataTitle: data.map((value) => [value.recipe_title]),
+      jsonDataIngredients: data.map((value) => [value.regist_ingredients]),
+      jsonDataRegister: data.map((value) => [value.recipe_register]),
+      jsonDataRecommend: data.map((value) => [value.recipe_recommend]),
+      jsonDataViews: data.map((value) => [value.recipe_views]),
+      jsonDataImg: data.map((value) => [value.thumbnail_img]),
+    };
+
+    const pageAll = {
+      total: data.length, //전체 게시글 갯수
+      pageContentCount: 4, //한페이지에 보여질 게시글 갯수
+      currPage: 1, //현재페이지
+      pageNumCount: 5, //중간 페이징 버튼 갯수
+    };
+
+    const numberListWrap = document.getElementById("number-list-wrap");
+    const recipeListWrap = document.getElementById("recipe-list-wrap");
+
+    renderButtonContainer(numberListWrap, recipeListWrap, pageAll, dataAll);
+  }
+};
+
+//db.json 파일 GET방식으로 오픈
+http.open("GET", "../JSON/recipe_list_data.json");
+http.send();
