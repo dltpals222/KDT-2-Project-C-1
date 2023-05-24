ce2e4d537bdb76fe85c1de375cdc652402d98482
leesemin <leesemin222@gmail.com>
2023-05-24 22:21:44 +0900

    필터함수 제작(임시)

--- prototype_mobile/common/common_header.js
+++ prototype_mobile/common/common_header.js
@@ -1,5 +1,5 @@
 import amEditor from "../module/all_mighty_editor.js";
-import { fetchData } from "./search.js";
+import { fetchData, filter } from "./search.js";
 
 const { multiAndSingleTagMaker, positionEditor, fontAndLayoutEditor, kingGodFlexEditor, allMightyStyleEditor } =
   amEditor;
@@ -214,4 +214,6 @@ const urls = ["../JSON/recipe_list_data.json", "../api_parse/processed_data_ingr
 Promise.all([fetchData(urls[0]), fetchData(urls[1])]).then((dataArr) => {
   const recipeListData = dataArr[0].map((value) => value.recipe_title);
   const recipeIngredients = dataArr[1].ingredients;
+  let inputValue = filter();
+  console.log(inputValue);
 });
--- prototype_mobile/common/search.js
+++ prototype_mobile/common/search.js
@@ -2,3 +2,8 @@ export async function fetchData(path) {
   const result = await fetch(path);
   return result.json();
 }
+
+export function filter() {
+  let inputValue = document.getElementById("hs-input").value;
+  return inputValue;
+}
